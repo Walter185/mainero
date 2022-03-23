d7e7a0cabdc37e101b83a246ceb08f4cf715f3e5
Walter185 <walter.liendo@gmail.com>
2022-03-23 10:35:50 -0300

    estilo

--- codigo.js
+++ codigo.js
@@ -1,11 +1,23 @@
-const contenedor  = document.querySelector(".flex-container");
+const contenedor = document.querySelector(".flex-container");
 
-function crearMaquina(nombre,modelo,precio){
-    img= "<img src='https://www.mainero.com/es/wp-content/uploads/2020/02/MDD200.jpg'>";
-    nombre= '<h2>${nombre}</h2>';
-    modelo= '<h3>${modelo}</h3>';
-    precio= '<p>Precio: <b>$${precio}</b></p>';
-    return [img,nombre,modelo,precio];
+function crearMaquina(nombre, modelo, precio) {
+    img = "<img src='https://www.mainero.com/es/wp-content/uploads/2020/02/MDD200.jpg'>";
+    nombre = '<h2>${nombre}</h2>';
+    modelo = '<h3>${modelo}</h3>';
+    precio = '<p>Precio: <b>$${precio}</b></p>';
+    return [img, nombre, modelo, precio];
 }
-const Maquina=crearMaquina("maquina","modelo","precio")
-document.write(Maquina[0],Maquina[1],Maquina[2])
+
+let documentFragment = document.createDocumentFragment();
+for (var i = 1; i <= 20; i++) {
+    let modeloRandom = Math.round(Math.random() * 10000);
+    let precioRandom = Math.round(Math.random() * 10 + 30);
+    let Maquina = crearMaquina('Maquina ${i}', 'modelo ${modeloRandom}', precioRandom);
+    let div = document.createElement("DIV");
+    div.classList.add('item-${i}', 'flex-item');
+    div.innerHTML = Maquina[0] + Maquina[1] + Maquina[2] + Maquina[3];
+    documentFragment.appendChild(div);
+
+}
+
+contenedor.appendChild(documentFragment);
