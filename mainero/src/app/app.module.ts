1360d087af9df5c96f2fdb700adcfa94f750f0aa
Walter185 <walter.liendo@gmail.com>
2022-03-27 20:58:45 -0300

    productos

--- mainero/src/app/app.module.ts
+++ mainero/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    BannerComponent,
+    FooterComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
