dbdf1228502ad5c5c0d2c78b4b9e51a261378bb6
Walter185 <walter.liendo@gmail.com>
2022-03-27 16:27:01 -0300

    angular

--- codigo.js
+++ codigo.js
@@ -7,17 +7,3 @@ function crearMaquina(nombre, modelo, precio) {
     precio = '<p>Precio: <b>$${precio}</b></p>';
     return [img, nombre, modelo, precio];
 }
-
-let documentFragment = document.createDocumentFragment();
-for (var i = 1; i <= 20; i++) {
-    let modeloRandom = Math.round(Math.random() * 10000);
-    let precioRandom = Math.round(Math.random() * 10 + 30);
-    let Maquina = crearMaquina('Maquina ${i}', 'modelo ${modeloRandom}', precioRandom);
-    let div = document.createElement("DIV");
-    div.classList.add('item-${i}', 'flex-item');
-    div.innerHTML = Maquina[0] + Maquina[1] + Maquina[2] + Maquina[3];
-    documentFragment.appendChild(div);
-
-}
-
-contenedor.appendChild(documentFragment);
